1cbb75449f33a63cc66f64d90dfe9ecdf927f791
Prathibha Ratnayake <60864062+prathibha97@users.noreply.github.com>
2020-11-17 16:09:28 +0530

    Add files via upload

---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,47 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import {deleteEducation} from '../../actions/profile'
+import Momemt from 'react-moment'
+
+const Education = ({ education ,deleteEducation}) => {
+    const educations = education.map((edu) => (
+        <tr key={edu._id}>
+            <td>{edu.school}</td>
+            <td className='hide-sm'>{edu.degree}</td>
+            <td>
+                <Momemt format='YYYY/MM/DD'>{edu.from}</Momemt> - {' '}
+                {
+                    edu.to === null ? (' Now') : (<Momemt format='YYYY/MM/DD'>{edu.to}</Momemt>)
+                }
+            </td>
+            <td>
+                <button className='btn btn-danger' onClick={()=>deleteEducation(edu._id)}>Delete</button>
+            </td>
+        </tr>
+    ))
+    return (
+        <Fragment>
+            <h2 className="my-2">Education Credentials</h2>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>School</th>
+                        <th className='hide-sm'>Degree</th>
+                        <th className='hide-sm'>Years</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>{educations}</tbody>
+            </table>
+        </Fragment>
+    )
+}
+
+Education.propTypes = {
+    education: PropTypes.array.isRequired,
+    deleteEducation: PropTypes.func.isRequired,
+}
+
+
+export default connect(null, {deleteEducation})(Education)
